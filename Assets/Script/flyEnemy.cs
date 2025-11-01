a048662d6fccf63049c2c9405b4bfd581167dd72
Redv123 <161664341+Redv123@users.noreply.github.com>
2025-11-01 12:58:00 -0400

    enemy movement

--- Assets/Script/flyEnemy.cs
+++ Assets/Script/flyEnemy.cs
@@ -1,16 +1,36 @@
 using UnityEngine;
+using System.Collections;
 
 public class flyEnemy : Unit
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private int speed = 6;
+    private float startPointX;
+    [SerializeField] private GameObject fireball;
     void Start()
     {
-        
+        startPointX = transform.position.x;
+        StartCoroutine(Fireball());
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float x = startPointX - speed * Time.fixedTime;
+        float y = 4f * Mathf.Sin(2f * Time.fixedTime);
+        transform.position = new Vector2(x, y);
+    }
+
+
+    private IEnumerator Fireball()
+    {
+        Instantiate(fireball, transform.position, Quaternion.identity);
+        yield return new WaitForSeconds(2f);
+        StartCoroutine(Fireball());
+    }
+
+
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
     }
 }
