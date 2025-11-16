364a058dee56405176743b99c67668c00f5dbb4b
Redv123 <161664341+Redv123@users.noreply.github.com>
2025-11-16 15:22:06 -0500

    Ballon movement

--- Assets/Script/BallonAction.cs
+++ Assets/Script/BallonAction.cs
@@ -1,7 +1,20 @@
+using UnityEngine;
+
 public class BallonAction : Unit
 {
+    public float speed = 7f;
+    public bool move = true;
+
     public void OnBecameInvisible()
     {
         Destroy(gameObject);
+    }
+
+    void FixedUpdate()
+    {
+        if (move)
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+        }
     }
 }
