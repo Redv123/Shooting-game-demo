c590d999d146109e9411b7d65e83ec108f21b810
Redv123 <161664341+Redv123@users.noreply.github.com>
2025-11-29 10:54:03 -0500

    Remove debug information

--- Assets/Script/Menue/Pause.cs
+++ Assets/Script/Menue/Pause.cs
@@ -15,14 +15,12 @@ public class Pause : MonoBehaviour
             {
                 Time.timeScale = 0f;
                 pause.SetActive(true);
-                Debug.Log("暂停成功。");
 
             }
             else
             {
                 Time.timeScale = 1f;
                 pause.SetActive(false);
-                Debug.Log("解除暂停成功。");
             }
         }
     }
@@ -30,6 +28,5 @@ public class Pause : MonoBehaviour
     void Start()
     {
         pause.SetActive(false);
-        Debug.Log("初始化成功。");
     }
 }
