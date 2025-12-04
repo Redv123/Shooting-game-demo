abb5d859cf7d79e9377361121815a1ff11dfc619
Redv123 <161664341+Redv123@users.noreply.github.com>
2025-12-04 11:39:31 -0500

    Update Level 2 enemy generation

--- Assets/Script/Enemy/GenerateEnemies.cs
+++ Assets/Script/Enemy/GenerateEnemies.cs
@@ -1,25 +1,37 @@
 using UnityEngine;
 using System.Collections;
 
-public class GenerateEnemies : MonoBehaviour
+public class GenerateBallon : MonoBehaviour
 {
-
-    private readonly WaitForSeconds shortWait = new(5f);
-    private float startPointX;
-    private float startPointY;
+    [SerializeField] private Transform[] startPoint;
     [SerializeField] private GameObject enemy;
+    private readonly WaitForSeconds shortWait = new(5f);
+    private float startPointX, startPointY;
+
     void Start()
     {
-        startPointX = transform.position.x;
-        startPointY = transform.position.y;
+        GameData.end = false; // reset level end state when level starts
+        StartCoroutine(End());
         StartCoroutine(Generate());
+        MusicManager.Instance.StopMusic();
     }
-    
-    private IEnumerator Generate()
+    IEnumerator Generate()
     {
-        startPointY += Random.Range(-5f, 20f);
-        Instantiate(enemy, new Vector2(startPointX,startPointY), Quaternion.identity);
-        yield return shortWait;
-        StartCoroutine(Generate());
+        while (!GameData.end)
+        {
+            int random = Random.Range(0, startPoint.Length);
+            startPointX = startPoint[random].position.x;
+            startPointY = startPoint[random].position.y;
+
+            Instantiate(enemy, new Vector2(startPointX, startPointY), Quaternion.identity);
+            yield return shortWait;
+        }
+    }
+
+    IEnumerator End()
+    {
+        yield return new WaitForSeconds(60f);
+        GameData.end = true;
+        Unit.CheckWin?.Invoke();
     }
 }
