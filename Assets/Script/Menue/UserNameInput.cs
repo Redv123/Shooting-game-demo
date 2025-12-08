27518fa89da4abec783fbcfa4e898872e17f6c43
Redv123 <161664341+Redv123@users.noreply.github.com>
2025-12-07 21:01:21 -0500

    Check username input

--- Assets/Script/Menue/UserNameInput.cs
+++ Assets/Script/Menue/UserNameInput.cs
@@ -1,20 +1,23 @@
 using TMPro;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UserNameInput : MonoBehaviour
 {
     [SerializeField] Button btn;
     [SerializeField] TMP_InputField input;
+    [SerializeField] GameObject warning;
+    public void Input()
+    {
+        btn.interactable = !string.IsNullOrWhiteSpace(input.text);
+        warning.SetActive(input.text.Length == 8);
+    }
+
+
     public void Confirm()
     {
         GameData.playerName = input.text;
         SceneManager.LoadScene("Start Menue");
-    }
-
-    public void Input()
-    {
-        btn.interactable = input.text != "";
     }
 }
